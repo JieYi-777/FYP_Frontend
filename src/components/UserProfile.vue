5eebc04ce2d8b8ee3b0b7d38b860917e358c65b4
JieYi-777 <limjieyi777@gmail.com>
2024-03-14 22:28:20 +0800

    User Profile (working in username request)

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,4 +1,7 @@
 <template>
+
+  <!-- The loading spinner -->
+  <Loading :isLoading="loading" />
   
   <!-- The username field -->
   <Fieldset class="profile_fieldset">
@@ -10,12 +13,37 @@
     </template>
     
     <div class="profile_fieldset_content">
-      <span class="text-lg">{{ current_username }}</span>
+      <span>{{ current_username }}</span>
 
-      <Button icon="pi pi-pencil" />
+      <Button icon="pi pi-pencil" size="small" v-tooltip.right="'Edit'" @click="open_EditUsername"/>
     </div>
     
   </Fieldset>
+
+  <!-- Because the new username input and validation text cannot clear, so need to clear when dialog is closed -->
+  <Dialog v-model:visible="editUsername_visible" modal :draggable="false" 
+    header="Edit Username" style="width:25rem;" @hide="clearNewUsername" @after-hide="clearNewUsernameValidationText">
+    <!-- New username input -->
+    <div>
+      <InputGroup>
+        <InputGroupAddon>
+          <i class="pi pi-user"></i>
+        </InputGroupAddon>
+
+        <InputText placeholder="New Username" v-model="newUsername" :class="{'p-invalid': newUsername_validationText}"/>
+      </InputGroup>
+    </div>
+
+    <!-- Validation respond to username input -->
+    <div :class="{'text-left mb-3': newUsername_validationText, 'mb-9': !newUsername_validationText}">
+      <small class="redText">{{ newUsername_validationText }}</small>
+    </div>
+
+    <div class="flex justify-end gap-2">
+      <Button type="button" label="Cancel" severity="secondary" @click="close_EditUsername"></Button>
+      <Button type="button" label="Save" @click="sendEditUsername"></Button>
+    </div>
+  </Dialog>
 
   <!-- The email field -->
   <Fieldset class="profile_fieldset">
@@ -27,9 +55,42 @@
     </template>
     
     <div class="profile_fieldset_content">
-      <span class="text-lg">{{ email }}</span>
+      <span>{{ current_email }}</span>
 
-      <Button icon="pi pi-pencil" />
+      <Button icon="pi pi-pencil" size="small" v-tooltip.right="'Edit'"/>
+    </div>
+    
+  </Fieldset>
+
+  <!-- The password field -->
+  <Fieldset class="profile_fieldset">
+    <template #legend>
+        <div class="profile_fieldset_title">
+          <Avatar icon="pi pi-lock" shape="circle" />
+          <span class="font-bold">Password</span>
+        </div>
+    </template>
+    
+    <div class="profile_fieldset_content">
+      <span>*******</span>
+
+      <Button icon="pi pi-key" size="small" v-tooltip.right="'Change Password'"/>
+    </div>
+    
+  </Fieldset>
+
+  <!-- The notification field -->
+  <Fieldset class="profile_fieldset">
+    <template #legend>
+        <div class="profile_fieldset_title">
+          <Avatar icon="pi pi-bell" shape="circle" />
+          <span class="font-bold p-1">Notification</span>
+          <i class="pi pi-info-circle self-start cursor-pointer" v-tooltip.right="'Budget Threshold Email'"/>
+        </div>
+    </template>
+    
+    <div class="profile_fieldset_content">
+      <InputSwitch v-model="notification_check" />
     </div>
     
   </Fieldset>
@@ -38,29 +99,103 @@
 </template>
 
 <script>
+import Loading from './Loading.vue';
 import Fieldset from 'primevue/fieldset';
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import Avatar from 'primevue/avatar';
+import InputSwitch from 'primevue/inputswitch';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
 
-import { getUsername } from '../composables/Profile';
+import { getUsername, clearNewUsername, controlEditUsernameDialog, clearValue } from '../composables/Profile';
 import { getUserEmail } from '../composables/UserEmail';
+import { usernameValidation, checkValidInput } from '../composables/UserRegisterValidation';
+import { controlLoading } from '../composables/Loading';
+import { ref } from 'vue';
+import { useStore } from 'vuex';
+import axios1 from '../axios.service';
 
 export default {
-  components: { Fieldset, Dialog, Button, InputText, Avatar },
+  components: { Loading, Fieldset, Dialog, Button, InputText, Avatar, InputSwitch, InputGroup, InputGroupAddon },
   setup() {
+
+    // To control the loading spinner
+    const { loading, startLoading, stopLoading } = controlLoading();
+
+
+    const store = useStore();
 
     // To get the username ref
     const { current_username } = getUsername();
 
-    // To get the email ref and validation
-    const { email, email_validationText, sendGetEmailRequest } = getUserEmail();
-    sendGetEmailRequest();
+    // To get the new username ref and validation text ref
+    const { username: newUsername, username_validationText: newUsername_validationText } =usernameValidation();
+
+    // To control the visibility of the Edit Username Dialog
+    const { editUsername_visible, open_EditUsername, close_EditUsername } = controlEditUsernameDialog();
+
+    // Create the email ref
+    const current_email = ref('');
+
+    // The notification check ref
+    const notification_check = ref(true);
+
+    // Define the get email function (to call the request)
+    const getEmail = async() => {
+      try {
+        // Call getUserEmail to fetch the email value
+        const { current_email: emailValue } = await getUserEmail();
+
+        current_email.value = emailValue.value;
+      } catch (error) {
+        console.error('Error fetching user email:', error);
+      }
+    };
+
+    // Call function to get the email
+    getEmail();
+
+    // To clear the new username input
+    const clearNewUsername = () => {
+      clearValue(newUsername);
+    }
+
+    // To clear the new username validation text
+    const clearNewUsernameValidationText = () => {
+      clearValue(newUsername_validationText);
+    }
+
+    // To send the edit username request
+    const sendEditUsername = () => {
+      // Check the new username has value or not
+      if(!newUsername.value){
+        newUsername_validationText.value = 'Please enter your username';
+      }
+
+      if(checkValidInput(newUsername.value, newUsername_validationText.value)){
+        // Start the loading spinner
+        startLoading();
+
+        // Collect data in object
+        const data = {
+          new_username: newUsername.value.trim()
+        }
+
+        // Get the token
+        const token = store.getters.getToken;
+
+        axios1.put('')
+      }
+    }
 
     return {
-      current_username,
-      email,
+      loading,
+      current_username, newUsername, newUsername_validationText, editUsername_visible, 
+      open_EditUsername, close_EditUsername, sendEditUsername, clearNewUsername, clearNewUsernameValidationText,
+      current_email,
+      notification_check
     };
     
   }
@@ -69,7 +204,7 @@ export default {
 
 <style>
 .profile_fieldset {
-  @apply border-2 border-blue-300 mb-5;
+  @apply border-2 border-slate-300 mb-5;
 }
 
 .profile_fieldset_title {
--- src/composables/Profile.js
+++ src/composables/Profile.js
@@ -1,8 +1,7 @@
 import { ref } from "vue";
 import { useStore } from "vuex";
 
-import { usernameValidation } from './UserRegisterValidation';
-
+// To get the username from the vuex
 export const getUsername = () => {
   // Ref to the username value
   const current_username = ref('');
@@ -14,4 +13,29 @@ export const getUsername = () => {
   current_username.value = store.getters.getUsername;
 
   return { current_username };
+}
+
+// To clear the ref value when the Dialog is closed
+export const clearValue = (ref_value) => {
+  ref_value.value = '';
+}
+
+// To control the visible of the Edit Username Dialog
+export const controlEditUsernameDialog = () => {
+
+  // The Edit Username Dialog ref
+  const editUsername_visible = ref(false);
+
+  // To open the Edit Username Dialog
+  const open_EditUsername = () => {
+    editUsername_visible.value = true;
+  }
+
+  // To close the Edit Username Dialog
+  const close_EditUsername = () => {
+    editUsername_visible.value = false;
+  }
+
+  return { editUsername_visible, open_EditUsername, close_EditUsername };
+
 }
