cfc51da70b2e45d95dfd66b8b02ec1375eb12bf0
JieYi-777 <limjieyi777@gmail.com>
2024-03-15 15:27:07 +0800

    Axios interceptor for token expiration

--- src/axios.service.js
+++ src/axios.service.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import router from './router';
 
 //const baseUrl = process.env.VUE_APP_API_URL || 'http://localhost:8000/api/'; // Replace with your actual API URL
 
@@ -9,5 +10,18 @@ const baseURL = process.env.VUE_APP_AXIOS_URL;
 const axios1 = axios.create({
   baseURL,
 });
+
+// Add interceptor for sending request, if get the response is expired token or unauthorized access, redirect to login page
+axios1.interceptors.response.use(
+  response => response,
+  error => {
+    // Check if the error is due to an expired token or unauthorized access
+    if (error.response && error.response.status === 401) {
+      // Redirect the user to the login page
+      router.push({ name: 'login' });
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axios1;
