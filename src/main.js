4a0e2587f1a726d016700e6d0fea0636c7cff62d
JieYi-777 <limjieyi777@gmail.com>
2024-04-04 09:13:33 +0800

    temporary disable tailwind

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router';
 import store from './store'
 
 // Import tailwind css
-import './assets/tailwind.css';
+// import './assets/tailwind.css';
 
 // Import PrimeVue UI framework
 import PrimeVue from 'primevue/config';
