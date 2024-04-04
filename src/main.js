98d767bf7429f75f0d0a8a05cef6251dd735c65d
JieYi-777 <limjieyi777@gmail.com>
2024-04-04 09:35:58 +0800

    Try tailwind primevue 2

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store'
 
+// Import tailwind css
+import './assets/tailwind.css';
+
 // Import PrimeVue UI framework
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/aura-light-blue/theme.css';
@@ -13,9 +16,6 @@ import Ripple from 'primevue/ripple';
 import BadgeDirective from 'primevue/badgedirective';
 import Tooltip from 'primevue/tooltip';
 import ConfirmationService from 'primevue/confirmationservice';
-
-// Import tailwind css
-import './assets/tailwind.css';
 
 // Apex Chart
 import VueApexCharts from "vue3-apexcharts";
